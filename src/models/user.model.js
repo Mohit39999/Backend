02c02dedc6d3d8e145986d694ba240a502efd13e
mohit <mohitsaraswat39999@gmail.com>
2024-11-16 20:33:55 +0530

    add user and video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,59 +1,83 @@
-import e from "express";
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-        index: true
-
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
     fullName: {
-        type: String,
-        required: true,
-        trim: true,
-        index: true
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
     },
     avatar: {
-        type: String,
-        trim: true,
-        required: true
-        
+      type: String,
+      required: true,
     },
     coverImage: {
-        type: String,
-        trim: true,
+      type: String,
     },
     password: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: [true, "Password is required"],
     },
     refreshToken: {
-        type: String,
-        trim: true
+      type: String,
     },
     watchHistory: [
-        {
-            videoId: {
-                type: Schema.Types.ObjectId,
-                ref: "Video"
-            }
-        }
-    ]
-    
-},{
-    timestamps: true});
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+userSchema.methods.isPassawordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+userSchema.methods.generateAccessToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign({ id: this._id }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+  });
+};
 
 export default mongoose.model("User", userSchema);
--- src/models/videos.model.js
+++ src/models/videos.model.js
@@ -1,4 +1,5 @@
 import mongoose,{Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema=new Schema({
     videoFile:{
@@ -24,17 +25,19 @@ const videoSchema=new Schema({
     },
     views:{
         type:Number,
-        required:true
+        default:0,
     },
     isPublished:{
         type:Boolean,
-        required:true
+        default:true
     },
     owner:{
         type:Schema.Types.ObjectId,
         ref:"User",
     }
 },{timestamps:true})
+
+videoSchema.plugin(mongooseAggregatePaginate)
 
 
 export default mongoose.model("Video",videoSchema)
